fe09a17bc9829cd788ee3b001ff4160da85ddeec
Salomon Beza <beza.salomon@gmail.com>
2017-01-19 15:57:20 +0200

    added a condition to control an empty name, and created the guessing game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,30 @@
 class GamesController < ApplicationController
 	def param_games
-		@display = params[:name]
-		@array = @display[0]
-	
-		if @array == "A"
-			@answer = "Hey, your name starts with the first letter of the alphabet!"
+
+		value = params[:name]
+		if value.class == String
+			@display = params[:name]
+			@array = @display[0]
+			if @array == "A"
+				@answer = "Hey, your name starts with the first letter of the alphabet!"
+			end
+			
+		else
+			@array = ""
 		end
-		render "param_game.html.erb"
+	render "param_game.html.erb"
+	end
+
+	def guess
+		correct_number = 39
+		result = params[:number]
+		if result.to_i > correct_number
+			@message = "Guess lower"
+		elsif result.to_i < correct_number
+			@message = "Guess higher"
+		else
+			@message = "Congz, you won"
+		end
+		render "guess_game.html.erb"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 	get "/games", to: 'games#param_games'
+	get "/guess", to: 'games#guess'
 end
